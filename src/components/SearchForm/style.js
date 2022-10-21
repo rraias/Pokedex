65d7625ff516b5e7555544a53750885448af8bcb
Raphael <raphaelraia@outlook.com.br>
2022-10-20 21:51:50 -0300

    responsiveness in pagination

--- src/components/SearchForm/style.js
+++ src/components/SearchForm/style.js
@@ -5,18 +5,25 @@ export const Form = styled.form`
     align-items: center;
     justify-content: center;
     width: 100%;
-    margin: 4%;
+    margin-top: 4%;
     gap: 0.5%;
 
     span{
       color: #ffcb05;
     }
+
 `;
 
 export const Label = styled.label`
     font-size: 1.5rem;
     font-weight: bolder;
     letter-spacing: 3px;
+
+    @media (max-width: 1129px) {
+    font-size: 0.8rem;
+    letter-spacing: 1px;
+    }
+
 `;
 
 export const TextInput = styled.input`
@@ -28,6 +35,10 @@ export const TextInput = styled.input`
     margin-right: 1%;
     padding: 0.5%;
     box-shadow: rgba(0, 0, 0, 0.2) 0px 10px 40px -7px;
+
+    @media (max-width: 1129px) {
+      width: 60%;
+    }
 `;
 
 export const Button = styled.button`
@@ -42,10 +53,16 @@ export const Button = styled.button`
     transition: 0.5s;
     box-shadow: rgba(0, 0, 0, 0.2) 0px 10px 40px -7px;
 
+    @media (max-width: 1129px) {
+              font-size: 0.5rem;
+              width: fit-content;
+    }
+
         &:hover,
         &:focus{
             scale: 1.1;
             background-color: #FF0000;
             color: #FFFFFF;
+
 }
 `;
